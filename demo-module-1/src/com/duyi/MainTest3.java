c9b36f2d2795138fd500994610a3863790bf72b3
Rechard <1044136908@qq.com>
2020-05-27 15:02:37 +0800

    test分支修改2

--- demo-module-1/src/com/duyi/MainTest3.java
+++ demo-module-1/src/com/duyi/MainTest3.java
@@ -2,4 +2,19 @@ package com.duyi;
 
 public class MainTest3 {
     private Integer age;
+
+    public MainTest3(Integer age) {
+        this.age = age;
+    }
+
+    public MainTest3() {
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
